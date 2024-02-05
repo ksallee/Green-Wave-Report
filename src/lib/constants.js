a45e77fe1c6dedde0281a3c67859f90fe82d9839
Kevin Sallee <kevin.sallee@gpltech.com>
2024-02-05 16:36:09 -0600

    add grey

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,17 +1,19 @@
 export const colors = {
+    grey: [192, 192, 192], // Grey
     red: [240, 128, 128], // Soft red
     orange: [240, 164, 100], // Soft orange
     green: [144, 238, 144], // Soft green
     blue: [173, 216, 230], // Light blue
     violet: [221, 160, 221], // Plum, softer violet
 };
-export const thresholds = [0, 550, 650, 800, 1000, 1200];
+export const thresholds = [0, 450, 550, 650, 800, 1000, 1200];
 
 
 export const thresholdColors = {
-    red: [thresholds[0], thresholds[1]],
-    orange: [thresholds[1], thresholds[2]],
-    green: [thresholds[2], thresholds[3]],
-    blue: [thresholds[3], thresholds[4]],
-    violet: [thresholds[4], thresholds[5]],
+    grey: [thresholds[0], thresholds[1]],
+    red: [thresholds[1], thresholds[2]],
+    orange: [thresholds[2], thresholds[3]],
+    green: [thresholds[3], thresholds[4]],
+    blue: [thresholds[4], thresholds[5]],
+    violet: [thresholds[5], thresholds[6]],
 }
